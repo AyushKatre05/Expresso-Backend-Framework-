d80cef04141deb8647f20ac65a8448bc4dc6ee40
Ayush Katre <ayushkatre1801@gmail.com>
2026-02-14 14:25:25 +0530

    Update main.cpp

--- HTTP/src/main.cpp
+++ HTTP/src/main.cpp
@@ -435,3 +435,65 @@ void handle_client(int client_fd) {
   ::close(client_fd);
   std::cout << "Client disconnected\n";
 }
+
+
+int main(int argc, char **argv) {
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  for (size_t i = 1; i < argc; ++ i) {
+    if (std::string(argv[i]) == "--directory" && i + 1 < argc) {
+      g_directory = argv[i + 1];
+      ++ i;
+    }
+  }
+  std::cout << "Logs from your program will appear here!\n";
+  int server_fd = ::socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+   std::cerr << "Failed to create server socket\n";
+   return 1;
+  }
+  int reuse = 1;
+  if (::setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    std::cerr << "setsockopt failed\n";
+    return 1;
+  }
+  
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(4221);
+  
+  if (::bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    std::cerr << "Failed to bind to port 4221\n";
+    return 1;
+  }
+  
+  int connection_backlog = 5;
+  if (::listen(server_fd, connection_backlog) != 0) {
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+  
+  std::cout << "Server is listening on port 4221...\n";
+  while (true) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+  
+    int client_fd = ::accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    
+    if (client_fd < 0) {
+      std::cerr << "Failed to accept client connection\n";
+      continue;
+    }
+
+    std::cout << "Client connected\n";
+    
+    std::thread client_thread(handle_client, client_fd);
+    client_thread.detach();
+  }
+
+  ::close(server_fd);
+  return 0;
+}
